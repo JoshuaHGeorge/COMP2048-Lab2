51bb3faf73a22a27e199412cc30a0cd2e87e042f
JoshuaHGeorge <jhgeorg1@lakeheadu.ca>
2019-09-14 17:31:31 -0400

    Add project files.

---
+++ COMP2048-Lab2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace COMP2048_Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
