52d577a3eeb17c89d33a95b0fd0091e594022abb
JoshuaHGeorge <jhgeorg1@lakeheadu.ca>
2019-09-16 10:37:44 -0400

    Did the entire lab

---
+++ COMP2048-Lab2/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2048_Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(string category)
+        {
+            // add selected category to VIewBag to display the brose page
+            ViewBag.category = "You selected category: " + category;
+            return View();
+        }
+    }
+}
